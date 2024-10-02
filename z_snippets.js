54166f9d42047a5d2d5710ab419d4bffbc043c26
florinfl <florinfl@yahoo.com>
2024-10-02 12:20:40 +0300

    Add files via upload

---
+++ z_snippets.js
@@ -0,0 +1,9 @@
+//console.log(table_parse.outerHTML) // pentru conversie element in text
+//main.append(table_parse)
+
+
+//v2
+// let elem = document.createElement('div') // pentru conversie text in element html
+// elem.innerHTML = data
+// let table = elem.querySelector('table')
+//main.append(table)
